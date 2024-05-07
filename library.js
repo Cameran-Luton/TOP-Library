43fc3927979c75492a85236c6f0d55c729fbc487
Cameran-Luton <cameranluton5@gmail.com>
2024-05-07 06:41:41 +0100

    Setup of library project

---
+++ library.js
@@ -0,0 +1,12 @@
+function Book(title, author, pages, read) {
+  this.title = title;
+  this.author = author;
+  this.pages = pages;
+  this.read = read;
+  this.info = function () {
+    return title + " by " + author + ", " + pages + " pages, " + read;
+  };
+}
+
+const book1 = new Book("Book", "A guy", 134, "not read");
+console.log(book1.info());
